3dbd388acfa73302893582fb3538f488a5817ced
admin <admin@MacBook-Air-4.local>
2026-09-09 19:15:35 +0800

    Match source logo gradient

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ function BusinessCard({
       <img
         aria-hidden="true"
         className="template-sheet"
-        src={assetUrl(`/templates/${template}-template.svg`)}
+        src={assetUrl(`/templates/${template}-template.svg?v=logo-fix-2`)}
         alt=""
       />
       <svg
